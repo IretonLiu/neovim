009e2190cc97e248ae104c6247847b71c3b5fb5f
Ireton Liu <iretonsama@gmail.com>
2024-01-23 16:03:24 +0200

    added telescope ignore patterns

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,10 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Find git files" }
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Search > ") })
 end, { desc = "Find string" })
+
+local telescope = require('telescope').setup({
+    defaults = {
+        file_ignore_patterns = { "node_modules", ".git", "env", "venv", "build", "dist", "target", "out", "__pycache__", ".cache", ".vscode", ".idea", ".clangd", ".clang-tidy", ".clang-format", ".ccls-cache", ".ccls" },
+    },
+})
+
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/ireton/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/ireton/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/ireton/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/ireton/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/ireton/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
+local package_path_str = "/home/arch/ireton/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?.lua;/home/arch/ireton/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?/init.lua;/home/arch/ireton/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?.lua;/home/arch/ireton/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/arch/ireton/.cache/nvim/packer_hererocks/2.1.1702233742/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,164 +76,164 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["autoclose.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/autoclose.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/autoclose.nvim",
     url = "https://github.com/m4xshen/autoclose.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["conform.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/conform.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/conform.nvim",
     url = "https://github.com/stevearc/conform.nvim"
   },
   ["copilot.vim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/copilot.vim",
     url = "https://github.com/github/copilot.vim"
   },
   ["diffview.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/diffview.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/diffview.nvim",
     url = "https://github.com/sindrets/diffview.nvim"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["markdown-preview.nvim"] = {
     loaded = false,
     needs_bufread = false,
     only_cond = false,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/opt/markdown-preview.nvim",
     url = "https://github.com/iamcco/markdown-preview.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-colorizer.lua"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-dap"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap",
     url = "https://github.com/mfussenegger/nvim-dap"
   },
   ["nvim-dap-ui"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
     url = "https://github.com/rcarriga/nvim-dap-ui"
   },
   ["nvim-dap-virtual-text"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
     url = "https://github.com/theHamsta/nvim-dap-virtual-text"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
     url = "https://github.com/nvim-tree/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/nvim-tree/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/playground",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/playground",
     url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope-dap.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/telescope-dap.nvim",
     url = "https://github.com/nvim-telescope/telescope-dap.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/ireton/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/home/arch/ireton/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   }
 }
